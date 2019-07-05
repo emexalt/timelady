6d47fda688e412b42f59c5c09aa698f2ab68bf2e
Em Folsom <emmeryn.dev@gmail.com>
2019-07-04 22:42:54 -0500

    token is now global, comments, debug level logging

--- timelady.py
+++ timelady.py
@@ -5,23 +5,28 @@
 import discord, datetime, logging
 from discord.ext import commands
 
-logging.basicConfig(level=logging.INFO)
+TOKEN = ""
+
+logging.basicConfig(level=logging.DEBUG)
 
 timelady = commands.Bot(command_prefix="$")
 
+#sets status of bot when server is running
 @timelady.event
 async def on_ready():
     print("The bot is ready!")
     await timelady.change_presence(activity=discord.Game(name="Making a bot!"))
 
+#$test to test whether bot can send msg
 @timelady.command()
 async def test(ctx):
     await ctx.send("hello, world!")
 
+#display time on $time cmd
 @timelady.command()
 async def time(ctx):
     time = str(datetime.datetime.now().time())
     await ctx.send("The time from your Telephone Company is: " + time, tts=True)
 
-timelady.run("TOKEN GOES HERE")
+timelady.run(TOKEN)
 
